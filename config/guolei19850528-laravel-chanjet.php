d491255846b60a4dca767fc10cad8e7f93e0369c
guolei <174000902@qq.com>
2024-12-04 08:09:52 +0800

    update

---
+++ config/guolei19850528-laravel-chanjet.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 作者:郭磊
+ * 邮箱:174000902@qq.com
+ * 电话:15210720528
+ * Git:https://github.com/guolei19850528/laravel-chanjet
+ */
+return [
+    'u8plus' => [
+        'your key' => [
+            'url' => '',
+        ],
+    ]
+];
